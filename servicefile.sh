ec9ddf0a72334065503b4119abb37c4857c73fed
Aakash Singh <aakashsingh717@gmail.com>
2025-11-30 16:48:55 -0500

    add service

---
+++ servicefile.sh
@@ -0,0 +1,43 @@
+sudo tee /etc/systemd/system/flask.service > /dev/null <<'EOF'
+[Unit]
+Description=Gunicorn instance to serve Flask app
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/app
+ExecStart=/usr/local/bin/gunicorn -w 4 -b 0.0.0.0:5000 app:app
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+### Service start
+
+sudo tee /etc/systemd/system/flask.service > /dev/null <<'EOF'
+[Unit]
+Description=Gunicorn instance to serve Flask app
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/app
+ExecStart=/home/ec2-user/.local/bin/gunicorn -w 4 -b 0.0.0.0:5000 app:app
+Restart=always
+Environment="PATH=/home/ec2-user/.local/bin:/usr/bin:/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable flask
+sudo systemctl restart flask
+sudo systemctl status flask
