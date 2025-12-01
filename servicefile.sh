f0efa4c3bb3aa3abcfaa62c8adb9b5f17f9b06f7
Aakash Singh <aakashsingh717@gmail.com>
2025-11-30 20:01:38 -0500

    bug fix

--- servicefile.sh
+++ servicefile.sh
@@ -36,6 +36,23 @@ WantedBy=multi-user.target
 EOF
 
 
+####
+sudo tee /etc/systemd/system/flask.service > /dev/null <<'EOF'
+[Unit]
+Description=Gunicorn instance to serve Flask app
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/app
+Environment="AWS_DEFAULT_REGION=us-east-2"
+ExecStart=/home/ec2-user/.local/bin/gunicorn -w 4 -b 0.0.0.0:5000 app:app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 
 sudo systemctl daemon-reload
 sudo systemctl enable flask
